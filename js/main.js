21884eafdfaaee651c94b8232be76c6ccd77f815
Priyanka Negi <prinegi100@gmail.com>
2017-03-07 23:30:43 -0800

    modify the main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 //listen for form submit
 document.getElementById('myForm').addEventListener('submit', saveBookmark);
 
+
 function saveBookmark(e){
 
     var siteName = document.getElementById('siteName').value;
@@ -11,9 +12,13 @@ function saveBookmark(e){
         url : siteUrl
     }
     
+    if(!siteName || !siteUrl){
+    alert('please fill in the form');
+    return false;
+}    
 
-    //local storage
-   if(localStorage.getItem('bookmarks') === null){
+ //local storage
+ if(localStorage.getItem('bookmarks') === null){
        //init array
        var bookmarks = [];
        //add to array
@@ -22,7 +27,63 @@ function saveBookmark(e){
        //set to localStorage
        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
    }
-   
-      //prevent form from submitting
+
+
+   else{
+       //get bookmarks from the local storage
+     var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+       //add bookmark to array
+       bookmarks.push(bookmark);
+
+       //reset back to local storage
+       localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+ }
+ 
+ 
+  //re-fetch the bookmarks
+  fetchBookmark();
+  //prevent form from submitting
   e.preventDefault();
+}
+
+
+
+function deleteBookmark(url){
+    //get bookmarks from localStorage
+    var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+     
+    //loop throughtout the bookmarks
+    for(var i=0; i< bookmarks.length; i++){
+        if(bookmarks[i].url == url){
+            //remove from array
+            bookmarks.splice(i, 1);
+        }
+    }
+    //re-start back to localStorage
+    localStorage.setItem('bookmarks',JSON.stringify(bookmarks));
+
+    //re-fetch the bookmarks
+    fetchBookmark();
+}
+
+function fetchBookmark(){
+    //get bookmarks from the local storage
+     var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+
+     //get output id
+     var bookmarksResults = document.getElementById('bookmarksResults');
+
+     //buid output
+     bookmarksResults.innerHTML=" "; 
+     for(var i=0;i< bookmarks.length; i++){
+         var name = bookmarks[i].name;
+         var url =  bookmarks[i].url;
+
+         bookmarksResults.innerHTML+= '<div class= "well">' +
+                                     '<h3>'+name+
+                                     '<a class="btn btn-default" target="_blank" href="'+url+'">Visit</a>'+
+                                     '<a onclick="deleteBookmark(\''+url+'\' )" class="btn btn-danger" target="_blank" href="#">Delete</a> '+
+                                     '</h3>'+ 
+                                     '</div>';
+     }
 }
